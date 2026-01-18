43cf8b7353591601b918d0beb1b7c61a1d6237a9
ItzEmoji <no-reply@itzemoji.com>
2026-01-18 20:20:10 +0100

    made changes and updated flake.lock

---
+++ hosts/cyril-nixos/grub.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }: 
+{
+  boot.loader.grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiSupport = true;
+    useOSProber = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+}
